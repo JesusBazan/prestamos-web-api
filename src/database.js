28d66af51e08eca509268705cd3f3a225bc89dbf
EmmanuelBazan <mqkz99@gmail.com>
2021-06-28 23:09:35 -0500

    Creacion de rutas para productos

--- src/database.js
+++ src/database.js
@@ -4,7 +4,7 @@ const mysqlConnection = mysql.createConnection({
   host: 'dbprestamos.c48curxpcfx0.us-east-2.rds.amazonaws.com',
   user: 'admin',
   password: 'admin1234',
-  database: 'prueba',
+  database: 'LENDERS',
   multipleStatements: true
 });
 
--- src/routes/productos.js
+++ src/routes/productos.js
@@ -5,7 +5,7 @@ const mysqlConnection  = require('../database.js');
 
 // GET all usuarios
 router.get('/productos', (req, res) => {
-  mysqlConnection.query('SELECT * FROM productoTest', (err, rows, fields) => {
+  mysqlConnection.query('SELECT * FROM producto', (err, rows, fields) => {
     if(!err) {
       res.json(rows);
     } else {
@@ -15,9 +15,9 @@ router.get('/productos', (req, res) => {
 });
 
 // GET An usuario
-router.get('/buscarUsuario/:correo', (req, res) => {
-  const { correo } = req.params; 
-  mysqlConnection.query('SELECT * FROM usuario WHERE correo = ?', [correo], (err, rows, fields) => {
+router.get('/buscarProducto/:id', (req, res) => {
+  const { id } = req.params; 
+  mysqlConnection.query('SELECT * FROM producto WHERE id_producto = ?', [id], (err, rows, fields) => {
     if (!err) {
       res.json(rows);
     } else {
@@ -27,9 +27,9 @@ router.get('/buscarUsuario/:correo', (req, res) => {
 });
 
 // DELETE An usuario
-router.delete('/eliminarUsuario/:id', (req, res) => {
+router.delete('/eliminarProducto/:id', (req, res) => {
   const { id } = req.params;
-  mysqlConnection.query('DELETE FROM usuario WHERE id_usuario = ?', [id], (err, rows, fields) => {
+  mysqlConnection.query('DELETE FROM producto WHERE id_producto = ?', [id], (err, rows, fields) => {
     if(!err) {
       res.json({status: 'Employee Deleted'});
     } else {
@@ -39,84 +39,41 @@ router.delete('/eliminarUsuario/:id', (req, res) => {
 });
 
 // INSERT An usuario
-router.post('/insertarUsuario', (req, res) => {
-  const {id, username, nombres, apellidos, correo, rol, contrasenia, foto} = req.body;
-  console.log(id, username, nombres, apellidos, correo, rol, contrasenia, foto);
+router.post('/insertarProducto', (req, res) => {
+  const {id, nombre, descripcion, fotografia, idCatalogo} = req.body;
+  console.log(id, nombre, descripcion, fotografia, idCatalogo);
   const query = `
     SET @id = ?;
-    SET @username = ?;
-    SET @nombres = ?;
-    SET @apellidos = ?;
-    SET @correo = ?;
-    SET @rol = ?;
-    SET @contrasenia = ?;
-    SET @foto = ?;
-    CALL AddOrEditUsuario(@id, @username, @nombres, @apellidos, @correo, @rol, @contrasenia, @foto);
+    SET @nombre = ?;
+    SET @descripcion = ?;
+    SET @fotografia = ?;
+    SET @idCatalogo = ?;
+    CALL AddOrEditProducto(@id, @nombre, @descripcion, @fotografia, @idCatalogo);
   `;
-  mysqlConnection.query(query, [id, username, nombres, apellidos, correo, rol, contrasenia, foto], (err, rows, fields) => {
+  mysqlConnection.query(query, [id, nombre, descripcion, fotografia, idCatalogo], (err, rows, fields) => {
     if(!err) {
-      res.json({status: 'Usuario Saved'});
+      res.json({status: 'Producto Saved'});
     } else {
       console.log(err);
     }
   });
-
 });
 
 // UPDATE An usuario
 router.put('/actualizarUsuario/:id', (req, res) => {
-  const { username, nombres, apellidos, correo, rol, contrasenia, foto } = req.body;
+  const { nombre, descripcion, fotografia, idCatalogo } = req.body;
   const { id } = req.params;
   const query = `
     SET @id = ?;
-    SET @username = ?;
-    SET @nombres = ?;
-    SET @apellidos = ?;
-    SET @correo = ?;
-    SET @rol = ?;
-    SET @contrasenia = ?;
-    SET @foto = ?;
-    CALL AddOrEditUsuario(@id, @username, @nombres, @apellidos, @correo, @rol, @contrasenia, @foto);
+    SET @nombre = ?;
+    SET @descripcion = ?;
+    SET @fotografia = ?;
+    SET @idCatalogo = ?;
+    CALL AddOrEditProducto(@id, @nombre, @descripcion, @fotografia, @idCatalogo);
   `;
-  mysqlConnection.query(query, [id, username, nombres, apellidos, correo, rol, contrasenia, foto], (err, rows, fields) => {
+  mysqlConnection.query(query, [id, nombre, descripcion, fotografia, idCatalogo], (err, rows, fields) => {
     if(!err) {
-      res.json({status: 'Usuario Updated'});
-    } else {
-      console.log(err);
-    }
-  });
-});
-
-
-// LOGEARSE
-router.post('/iniciarSesion', (req, res) => {
-  const {username, contrasenia} = req.body;
-  console.log(username, contrasenia);
-  const query = `
-  SET @username = ?;
-  SET @contrasenia = ?;
-  CALL IniciarSesion2(@username,@contrasenia);
-  `;
-  mysqlConnection.query(query, [username, contrasenia], (err, rows, fields) => {
-    if(!err) {
-      res.json(rows[2]);
-    } else {
-      console.log(err);
-    }
-  });
-});
-
-// TABLA VISTA USUARIOS
-router.post('/getTablaUsuarios', (req, res) => {
-  const {rol} = req.body;
-  console.log(rol);
-  const query = `
-  SET @rol = ?;
-  CALL GetTableUsuarios(@rol);
-  `;
-  mysqlConnection.query(query, [rol], (err, rows, fields) => {
-    if(!err) {
-      res.json(rows[1]);
+      res.json({status: 'Producto Updated'});
     } else {
       console.log(err);
     }
